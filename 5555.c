16314354b1b51d8f6d57226cafe95f7fee488239
b6320500549 <jirasin.po@ku.th>
2021-03-01 17:40:38 +0700

    5555.c,5555.exe,5555.o

---
+++ 5555.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+ char w[50];
+ int n,i;
+ scanf("%d",&n);
+ if(n>0&&n<100)
+ {
+ for(i=0;i<n;i++)
+ {
+     scanf("%s",w);
+ }
+
+ for(i=0;w[i]!='\0';i++)
+ {
+
+ }
+ }
+
+}
