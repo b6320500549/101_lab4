82c577883a09211d8fa38ac7e556f5242e65b4a4
b6320500549 <jirasin.po@ku.th>
2021-03-01 18:53:34 +0700

    4444.o,4444.exe,4444.c

---
+++ 4444.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main() {
+  char sr[1000] , g[10][5]={"i", "of", "the","on", "at", "for", "with", "a", "an" , "in" } ;
+  char *m;
+  int i=0,j,k=0;
+  scanf("%s",sr);
+  m= strtok(sr," ");
+  while(m != NULL) {
+        for(j=0;j<10;j++)
+          if(strcmp(m,g[j]))
+            k++;
+        if(k==10||i==0)
+          printf("%c",toupper(m[0]));
+    m = strtok(NULL," ");
+    i++, k=0;
+}
+return 0;
+}
