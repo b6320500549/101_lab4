9b40a4ae1ab9f6e4745d13331e1d6905aa984370
b6320500549 <jirasin.po@ku.th>
2021-03-01 18:24:37 +0700

    Update 5555.c

--- 5555.c
+++ 5555.c
@@ -2,20 +2,88 @@
 #include<string.h>
 int main()
 {
- char w[50];
- int n,i;
- scanf("%d",&n);
- if(n>0&&n<100)
- {
- for(i=0;i<n;i++)
- {
-     scanf("%s",w);
- }
+    int n,i,j,o,u=0;
+    scanf("%d",&n);
+    if(n>0&&n<100)
+    {
+        char w[n][50];
+        for(i=0; i<n; i++)
+        {
+            scanf("%[^\n]s",w[i]);
+        }
+        for(i=0; i<n; i++)
+        {
+            for(j=0; w[j]!='\0'; j++)
+            {
 
- for(i=0;w[i]!='\0';i++)
- {
+            }
+            for(i=0; i<n; i++)
+            {
+                for(j=0; w[j]!='\0'; j++)
+                    if(w[i][j]>='A'&&w[i][j]<='E')
+                    {
+                        if(w[i][j]=='A')
+                        {
+                            if(o==1)
+                                o=1;
+                            else if(o==2)
+                                o=3;
+                            else if(o==3)
+                                o=2;
+                            else
+                                o=4;
+                        }
+                        else if(w[i][j]=='B')
+                        {
+                            if(o==1)
+                                o=4;
+                            else if(o==2)
+                                o=2;
+                            else if(o==3)
+                                o=3;
+                            else
+                                o=1;
+                        }
+                        else if(w[i][j]=='C')
+                        {
+                            if(o==1)
+                                o=3;
+                            else if(o==2)
+                                o=4;
+                            else if(o==3)
+                                o=1;
+                            else
+                                o=2;
+                        }
+                        else if(w[i][j]=='D')
+                        {
+                            if(o==1)
+                                o=2;
+                            else if(o==2)
+                                o=1;
+                            else if(o==3)
+                                o=4;
+                            else
+                                o=3;
+                        }
+                        else if(w[i][j]=='E')
+                        {
+                            if(o==1)
+                                o=4;
+                            else if(o==2)
+                                o=3;
+                            else if(o==3)
+                                o=2;
+                            else
+                                o=1;
+                        }
 
- }
- }
+                    }
+                    else
+                        break;
 
-}
+            }
+            printf("%d\n",o);
+        }
+        return 0;
+    }
